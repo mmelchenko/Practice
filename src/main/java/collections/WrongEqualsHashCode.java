72a1b05a696366f0adc551a9e3919aeafbae6aad
Anatoly Trofimov <anatrof@gmail.com>
2015-04-28 11:51:32 +0300

    update

--- src/main/java/collections/WrongEqualsHashCode.java
+++ src/main/java/collections/WrongEqualsHashCode.java
@@ -2,8 +2,10 @@ package collections;
 
 /**
  * Created by odudak on 28.04.15.
+ * проверить как влияет на хешированные коллекции неправильная реализация хеш кода и эквалс
  */
-public class WrongEqualsHashCode {
+abstract public class WrongEqualsHashCode {
 
+    abstract void putAndCantGet();
 
 }
