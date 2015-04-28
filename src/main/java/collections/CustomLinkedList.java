b38fe118334c728e24e7659ade3f6d16ed109b32
Anatoly Trofimov <anatrof@gmail.com>
2015-04-28 11:48:49 +0300

    update list

--- src/main/java/collections/CustomLinkedList.java
+++ src/main/java/collections/CustomLinkedList.java
@@ -2,6 +2,29 @@ package collections;
 
 /**
  * Created by odudak on 28.04.15.
+ * имплементируй методы и убери абстракт
  */
-public class CustomLinkedList {
+abstract public class CustomLinkedList<T> {
+    private T data;
+    private CustomLinkedList<T> next;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public CustomLinkedList<T> getNext() {
+        return next;
+    }
+
+    public void setNext(CustomLinkedList<T> next) {
+        this.next = next;
+    }
+
+    abstract void appendToEnd(T element);
+
+    abstract void pushOnTop(T element);
 }
