a439c7ca39094b8b2f4daefdc00a7f1130a72296
Melchenko Maksym <melchenkomaksym@gmail.com>
2015-05-06 19:14:48 +0300

    Concat.class accomplished. Test for it is done too.

--- src/main/java/base/strings/Concat.java
+++ src/main/java/base/strings/Concat.java
@@ -5,4 +5,31 @@ package base.strings;
  * убедиться что конкатенация жрёт память
  */
 public class Concat {
+
+    private String string;
+    private long freeMemory = Runtime.getRuntime().freeMemory();
+
+    public Concat(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void memoryExpense(int iterations) {
+
+        long endFreeMemory = 0;
+
+        for (int i = 0; i < iterations; i++) {
+            string += i;
+            System.out.println(Runtime.getRuntime().freeMemory());
+            if (i + 1 == iterations) {
+                endFreeMemory = Runtime.getRuntime().freeMemory();
+                System.out.println(endFreeMemory);
+            }
+        }
+
+        System.out.println("Concatenation of " + iterations + " strings takes memory: " + (freeMemory - endFreeMemory));
+    }
 }
