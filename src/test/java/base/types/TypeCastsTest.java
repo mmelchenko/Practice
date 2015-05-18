ab2945e03a4f57a192c7f391067d96d3e69764ed
Anatoly Trofimov <anatrof@gmail.com>
2015-05-18 16:21:40 +0300

    смотрим тему про женерики и разбираемся

--- src/test/java/base/types/TypeCastsTest.java
+++ src/test/java/base/types/TypeCastsTest.java
@@ -2,34 +2,33 @@ package base.types;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TypeCastsTest {
 
     @Test
     public void testCastMe() throws Exception {
-        TypeCasts<Number> ntc = new TypeCasts<Number>();
-        Object i = ntc.castMe(new Integer(1));
+        Number i = new TypeCasts<Number>().castMe(new Integer(1));
         System.out.println(i);
         assertTrue(i instanceof Number);
-        Object o = ntc.castMe("qwert");
+        CharSequence o = new TypeCasts<CharSequence>().castMe("qwert");
         System.out.println(o);
-        assertFalse(o instanceof Number);
-        Object b = ntc.castMe(new Byte("125"));
+        assertTrue(o instanceof CharSequence);
+        Number b = new TypeCasts<Number>().castMe(new Byte("125"));
         System.out.println(b);
-        assertTrue(b instanceof Byte);
-        Object s = ntc.castMe(new Short("10000"));
+        assertTrue(b instanceof Number);
+        Number s = new TypeCasts<Number>().castMe(new Short("10000"));
         System.out.println(s);
-        assertTrue(s instanceof Short);
-        Object l = ntc.castMe(new Long(200000));
+        assertTrue(s instanceof Number);
+        Number l = new TypeCasts<Number>().castMe(new Long(200000));
         System.out.println(l);
-        assertTrue(l instanceof Long);
-        Object t = ntc.castMe(new Boolean("true"));
+        assertTrue(l instanceof Number);
+        Comparable t = new TypeCasts<Comparable>().castMe(new Boolean("true"));
         System.out.println(t);
-        assertTrue(t instanceof Boolean);
-        Object c = ntc.castMe(new Character('\u0001'));
+        assertTrue(t instanceof Comparable);
+        Comparable c = new TypeCasts<Comparable>().castMe(new Character('\u0001'));
         System.out.println(c);
+        assertTrue(c instanceof Comparable);
     }
 
 }
