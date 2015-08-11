74fb2291b6d511d627abd5b799f3779194766e59
Melchenko Maksym <melchenkomaksym@gmail.com>
2015-08-11 20:10:43 +0300

    WrongEqualsHashCode class is updated. I don't know what should I do with putAndCantGet() method. So I've create a main method in this class. Take a look and specify more information, please.

--- src/main/java/collections/WrongEqualsHashCode.java
+++ src/main/java/collections/WrongEqualsHashCode.java
@@ -4,8 +4,37 @@ package collections;
  * Created by odudak on 28.04.15.
  * проверить как влияет на хешированные коллекции неправильная реализация хеш кода и эквалс
  */
-abstract public class WrongEqualsHashCode {
+public class WrongEqualsHashCode {
 
-    abstract void putAndCantGet();
+    CustomLinkedList<Integer> first = new CustomLinkedList<Integer>();
+    CustomLinkedList<Integer> second = new CustomLinkedList<Integer>();
 
+    public void init() {
+        for (int i = 1; i < 12; i++) {
+            first.appendToEnd(i);
+            second.appendToEnd(i);
+        }
+    }
+
+    public CustomLinkedList<Integer> getFirst() {
+        return first;
+    }
+
+    public CustomLinkedList<Integer> getSecond() {
+        return second;
+    }
+
+    public void putAndCantGet() {
+
+    }
+
+    public static void main(String[] args) {
+        WrongEqualsHashCode test = new WrongEqualsHashCode();
+        test.init();
+        CustomLinkedList<Integer> f = test.getFirst();
+        CustomLinkedList<Integer> s = test.getSecond();
+
+        System.out.println("Hash code verification: " + (f.hashCode() == s.hashCode()));
+        System.out.println("Equals verification: " + f.equals(s));
+    }
 }
